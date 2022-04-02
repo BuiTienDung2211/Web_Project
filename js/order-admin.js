ab4ff122e4ec261355a690d6faf5d11c7c6553fa
BuiTienDung2211 <2051063830@e.tlu.edu.vn>
2022-04-02 16:22:51 +0700

    finish and upload file admin-dashboard to github

---
+++ js/order-admin.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Chuột máy tính',
+        productNumber: '15000',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Màn hình LED',
+        productNumber: '4599',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Bán phím cơ',
+        productNumber: '12932',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Vỏ case máy tính',
+        productNumber: '2622',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Card màn hình',
+        productNumber: '1283',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+]
